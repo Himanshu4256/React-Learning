dc215e24d6c4937caeef89c2456929ea1fa740be
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2026-03-29 13:24:07 +0530

    commit changes

--- react-gaming/src/App.tsx
+++ react-gaming/src/App.tsx
@@ -1,9 +1,21 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-   const items = ["Apple", "Banana", "Mango","Orange","Papaya"];
-
-  return <><ListGroup items={items} heading = "Cities"></ListGroup></>
+  const items = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+  const [alertVisible,setAlertVisible] = useState(false);
+  return (
+    <>
+    {alertVisible && <Alert>My Alert</Alert>}
+      {/* <ListGroup items={items} heading="Cities" /> */}
+      {/* <Alert>Hello World</Alert> */}
+      <Button onClick={() => setAlertVisible(true)}>
+        Click Me
+      </Button>
+    </>
+  );
 }
 
-export default App; 
+export default App;
